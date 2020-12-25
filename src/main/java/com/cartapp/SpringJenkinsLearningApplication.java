a4e7fbb4d900f6fa1449e081c54ccffe6451435f
unknown <chandresh.02vikram@gmail.com>
2020-12-25 11:37:00 +0530

    push to git

---
+++ src/main/java/com/cartapp/SpringJenkinsLearningApplication.java
@@ -0,0 +1,14 @@
+package com.cartapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsLearningApplication.class, args);
+		System.out.println("welcome to jenkins tutorial ");
+	}
+
+}
